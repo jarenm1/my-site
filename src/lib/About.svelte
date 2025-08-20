135b3aba30e892f833c8993eab25c3f446bbfd1e
jarenm1 <jarenmchugh@gmail.com>
2025-08-20 05:38:19 -0500

    added music

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -5,7 +5,7 @@
     <h2 class="text-xl font-semibold mb-4">About Me</h2>
     <div class="space-y-2">
         <p class="text-xs text-white/60">cs student. austin tx. rust.</p>
-        <p>My favorite games are Minecraft and Factorio. I love engineering and building things. I also enjoy listening to and making music.</p>
+        <p>My favorite games are Minecraft and Factorio. I love engineering and building things. I also enjoy listening to <a class="underline" href="/music">music</a>.</p>
         <br>
         <p class="font-bold text-sm">contact me through X / twitter dms</p>
     </div>
---
+++ src/routes/music/+page.svelte
@@ -0,0 +1,28 @@
+<h1 class="text-2xl text-center mb-4">Artists I Like</h1>
+<ul class="list-inside text-md space-y-1">
+    <li>kasper gem</li>
+    <li>glaive</li>
+    <li>2hollis</li>
+    <li>aldn</li>
+    <li>kurtains</li>
+    <li>kmoe</li>
+    <li>kuru</li>
+    <li>Jane Remover</li>
+    <li>twikipedia</li>
+    <li>Pitfall</li>
+    <li>Wido</li>
+    <li>tropes</li>
+    <li>saturn</li>
+    <li>nettspend</li>
+    <li>fakemink</li>
+    <li>Ecco2k</li>
+    <li>Bladee</li>
+    <li>brakence</li>
+    <li>heavn</li>
+    <li>osquinn</li>
+    <li>Lil Shine</li>
+    <li>quannnic</li>
+    <li>SEBii</li>
+    <li>Harto Falion</li>
+    <li>lieu</li>
+</ul>
